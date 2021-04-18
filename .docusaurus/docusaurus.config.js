74ef3c74dc51ce0c1e01ad435348e57ff6589d8e
Torquil <torquil2@harkness.co.uk>
2021-04-18 20:21:55 +0200

    Create docusaurus.config.js

---
+++ .docusaurus/docusaurus.config.js
@@ -0,0 +1,119 @@
+export default {
+  "title": "TestQuality Documentation",
+  "tagline": "TestQuality Test Management Documentation",
+  "url": "https://doc.testquality.com/",
+  "baseUrl": "/",
+  "onBrokenLinks": "throw",
+  "onBrokenMarkdownLinks": "warn",
+  "favicon": "img/favicon.ico",
+  "organizationName": "TestQuality",
+  "projectName": "docusaurus",
+  "themeConfig": {
+    "navbar": {
+      "title": "TestQuality Docs",
+      "logo": {
+        "alt": "TestQuality Logo",
+        "src": "img/logo.svg"
+      },
+      "items": [],
+      "hideOnScroll": false
+    },
+    "footer": {
+      "style": "dark",
+      "links": [
+        {
+          "title": "Docs",
+          "items": [
+            {
+              "label": "Getting Started",
+              "to": "docs/"
+            }
+          ]
+        },
+        {
+          "title": "Community",
+          "items": [
+            {
+              "label": "Stack Overflow",
+              "href": "https://stackoverflow.com/questions/tagged/docusaurus"
+            },
+            {
+              "label": "Discord",
+              "href": "https://discordapp.com/invite/docusaurus"
+            },
+            {
+              "label": "Twitter",
+              "href": "https://twitter.com/docusaurus"
+            }
+          ]
+        },
+        {
+          "title": "More",
+          "items": [
+            {
+              "label": "Blog",
+              "to": "blog"
+            },
+            {
+              "label": "GitHub",
+              "href": "https://github.com/facebook/docusaurus"
+            }
+          ]
+        }
+      ],
+      "copyright": "Copyright © 2021 My Project, Inc. Built with Docusaurus."
+    },
+    "colorMode": {
+      "defaultMode": "light",
+      "disableSwitch": false,
+      "respectPrefersColorScheme": false,
+      "switchConfig": {
+        "darkIcon": "🌜",
+        "darkIconStyle": {},
+        "lightIcon": "🌞",
+        "lightIconStyle": {}
+      }
+    },
+    "docs": {
+      "versionPersistence": "localStorage"
+    },
+    "metadatas": [],
+    "prism": {
+      "additionalLanguages": []
+    },
+    "hideableSidebar": false
+  },
+  "presets": [
+    [
+      "@docusaurus/preset-classic",
+      {
+        "docs": {
+          "sidebarPath": "/Users/torquilharkness/Documents/TQ Files/Docusaurus/Git Clone Docusaurus/test-quality-doc/sidebars.js",
+          "routeBasePath": "/",
+          "editUrl": "https://github.com/facebook/docusaurus/edit/master/website/"
+        },
+        "blog": {
+          "showReadingTime": true,
+          "editUrl": "https://github.com/facebook/docusaurus/edit/master/website/blog/"
+        },
+        "theme": {
+          "customCss": "/Users/torquilharkness/Documents/TQ Files/Docusaurus/Git Clone Docusaurus/test-quality-doc/src/css/custom.css"
+        }
+      }
+    ]
+  ],
+  "baseUrlIssueBanner": true,
+  "i18n": {
+    "defaultLocale": "en",
+    "locales": [
+      "en"
+    ],
+    "localeConfigs": {}
+  },
+  "onDuplicateRoutes": "warn",
+  "customFields": {},
+  "plugins": [],
+  "themes": [],
+  "titleDelimiter": "|",
+  "noIndex": false
+};
