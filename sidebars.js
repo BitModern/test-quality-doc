eb5379acec1b6b307b979aa2abc26f47d4daeba5
Shanu Mandot <shanu@bitmodern.com>
2026-03-14 23:06:03 +0700

    fixed the left nav bar

--- sidebars.js
+++ sidebars.js
@@ -1,127 +1,122 @@
 module.exports = {
   docs: [
-      {
-       type: 'category',
+    {
+      type: 'category',
       label: 'Getting started',
       items: [
-            'getting-started/quick-start',
-            'getting-started/key-concepts',
-           ]
-},
-      {
-        type: 'category',
-        label: 'Migration & Imports',
-        items: [
-          'migration-imports/TestRail',
-          'importing_data',
-          'migration-imports/bdd_gherkin',
-           ]
-      },
-      {
-        type: 'category',
-        label: 'TestQuality CLI',
-        items: [
-          'testquality_cli/overview',
-          'testquality_cli/reference',
-           ]
-      },
-      {
-        type: 'category',
-        label: 'Automations',
-        items: [
-            {
-            type: 'category',
-            label: 'Test Runners Integration',
-            items:[
-                'automations-imports/test-runners/integrating_with_Cucumber',
-                'automations-imports/test-runners/integrating_with_Cypress',
-                'automations-imports/test-runners/integrating_with_Jasmine',
-                'automations-imports/test-runners/integrating_with_Jest',
-                'automations-imports/test-runners/integrating_with_JUnit',
-                'automations-imports/test-runners/integrating_with_Mocha',
-                'automations-imports/test-runners/integrating_with_Nightwatch',
-                'automations-imports/test-runners/integrating_with_NUnit',
-                'automations-imports/test-runners/integrating_with_PHPUnit',
-                'automations-imports/test-runners/integrating_with_Playwright',
-                'automations-imports/test-runners/integrating_with_Postman',
-                'automations-imports/test-runners/integrating_with_Pytest',
-                'automations-imports/test-runners/integrating_with_Robot Framework',
-                'automations-imports/test-runners/integrating_with_RSpec',
-                'automations-imports/test-runners/integrating_with_Selenium',
-                'automations-imports/test-runners/integrating_with_TestNG',
-                'automations-imports/test-runners/integrating_with_WebdriverIO',
-                'automations-imports/test-runners/integrating_with_xUnit',
-                 ]
-            },
-              ]
+        'getting-started/quick-start',
+        'getting-started/key-concepts',
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Projects & test plans',
+      items: [
+        'project',
+        'test_plans',
+        'overview',
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Design',
+      items: [
+        'tests',
+        'cycles',
+        'stories',
+        'shared_steps',
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Testing',
+      items: [
+        'run',
+        'explorations',
+        'test_drawer',
+        'filtering',
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Reporting & insights',
+      items: [
+        'analyze',
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Automations',
+      items: [
+        {
+          type: 'category',
+          label: 'Test runners',
+          items: [
+            'automations-imports/test-runners/integrating_with_Cucumber',
+            'automations-imports/test-runners/integrating_with_Cypress',
+            'automations-imports/test-runners/integrating_with_Jasmine',
+            'automations-imports/test-runners/integrating_with_Jest',
+            'automations-imports/test-runners/integrating_with_JUnit',
+            'automations-imports/test-runners/integrating_with_Mocha',
+            'automations-imports/test-runners/integrating_with_Nightwatch',
+            'automations-imports/test-runners/integrating_with_NUnit',
+            'automations-imports/test-runners/integrating_with_PHPUnit',
+            'automations-imports/test-runners/integrating_with_Playwright',
+            'automations-imports/test-runners/integrating_with_Postman',
+            'automations-imports/test-runners/integrating_with_Pytest',
+            'automations-imports/test-runners/integrating_with_Robot Framework',
+            'automations-imports/test-runners/integrating_with_RSpec',
+            'automations-imports/test-runners/integrating_with_Selenium',
+            'automations-imports/test-runners/integrating_with_TestNG',
+            'automations-imports/test-runners/integrating_with_WebdriverIO',
+            'automations-imports/test-runners/integrating_with_xUnit',
+          ]
         },
-      {
+      ]
+    },
+    {
       type: 'category',
       label: 'Integrations',
       items: [
         'integrate_with_jira',
         'integrate_with_github',
         'integrate_with_slack',
-        {
-          type: 'category',
-          label: 'Defect Management',
-          items:[
-              'defect_management',
-               ]
-      },
+        'defect_management',
       ]
-      },
-      {
-        type: 'category',
-        label: 'TestQuality API',
-        items: [
-          'api',
-              ]
-      },
-      {
+    },
+    {
       type: 'category',
-      label: 'User Guide',
+      label: 'Migration & imports',
       items: [
-          'test_plans',
-          'project',
-          'overview',
-          'tests',
-
-          
-          'cycles',
-          'stories',
-          'run',
-          'explorations',
-          'analyze',
-
-          {
-              type: 'category',
-              label: 'Test Management',
-              items:
-                  ['filtering',
-                      'test_drawer',
-                      'importing',
-                      'shared_steps',
-
-                  ],
-          },
-          {
-            type: 'category',
-            label: 'Administration',
-            items:
-                ['administration/profile',
-                    'administration/company',
-                     'administration/users',
-                    'administration/roles',
-                    'administration/watches',
-                    'administration/integration_config',
-                    'administration/lookup_data',
-                    'administration/customization',
-                    'administration/environments',
-                    'administration/subscriptions',
-                ],
-          },      
-      ],
+        'migration-imports/TestRail',
+        'importing_data',
+        'migration-imports/bdd_gherkin',
+      ]
+    },
+    {
+      type: 'category',
+      label: 'API & CLI',
+      items: [
+        'api',
+        'testquality_cli/overview',
+        'testquality_cli/reference',
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Settings',
+      items: [
+        'administration/profile',
+        'administration/company',
+        'administration/users',
+        'administration/roles',
+        'administration/watches',
+        'administration/integration_config',
+        'administration/lookup_data',
+        'administration/customization',
+        'administration/environments',
+        'administration/subscriptions',
+      ]
     },
     'whats_new',
   ],
